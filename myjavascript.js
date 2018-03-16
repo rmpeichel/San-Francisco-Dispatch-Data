56fabdf80c4790f3e18428de97adbecdb9f10b87
rmpeichel <37384798+rmpeichel@users.noreply.github.com>
2018-03-16 09:35:48 -0500

    test

--- myjavascript.js
+++ myjavascript.js
@@ -1,6 +1,6 @@
 function openPage(pageName, elmnt, color) {
     // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
+	var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -9,7 +9,7 @@ function openPage(pageName, elmnt, color) {
     // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
+		tablinks[i].style.backgroundColor = "";
     }
 
     // Show the specific tab content
